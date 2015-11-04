799703f41ac155d63c8bbd7b06b915719adc8eaa
Layton Nelson <notyal.dev@gmail.com>
2015-11-04 00:55:06 -0600

    Add color output

--- mirror.sh
+++ mirror.sh
@@ -23,12 +23,39 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR="$SCRIPT_DIR/repos"
-horizontal_line="----------------------------------------------------------------------"
 
-# define failure messages:
-WARN_cannot_update_remote="[WARN]: Could not run: 'git remote update'."
-ERROR_no_git_repos="Cannot find any git repos in dir: '$DIR'."
-ERROR_cannot_cd_repo="Cannot enter directory for repo."
+# enable colors
+alias echo='echo -e'
+
+# define literal colors:
+c=$'\e[0m' # reset
+cblk=$'\e[0;30m'
+cred=$'\e[0;31m'
+cgrn=$'\e[0;32m'
+cylw=$'\e[0;33m'
+cblu=$'\e[0;34m'
+cpur=$'\e[0;35m'
+ccyn=$'\e[0;36m'
+cwht=$'\e[0;37m'
+
+# colorscheme:
+cWARN=$cylw
+cERROR=$cred
+cINFO=$cblu
+cLINE=$cgrn
+cMARK=$cpur
+cDIR=$ccyn
+cCMD=$cred
+
+# define strings:
+horizontal_line="${cLINE}----------------------------------------------------------------------${c}"
+_m="${cMARK}@${c}"  # program output mark
+_f="${cMARK}\$${c}" # program finished mark
+
+# define messages:
+WARN_cannot_update_remote="${cWARN}[WARN]: Could not run:${c} '${cCMD}git remote update${c}'${cWARN}.${c}"
+ERROR_no_git_repos="${cWARN}Cannot find any git repos in dir${c}: '${cDIR}$DIR${c}'${cWARN}.${c}"
+ERROR_cannot_cd_repo="${cERROR}Cannot enter directory for repo.${c}"
 
 # functions:
 catch_failure(){
@@ -50,24 +77,32 @@ not_implemented(){
 
 print_help(){
 	local program="$1"
+
+	local h=${cylw} # headings
+	local o=${cblu} # options
+	local a=${cred} # args
+	local e=${cgrn} # extra args
+
+	local i='' # fix indent/alignment
+
 	cat <<EOF
 mirror.sh: Keep a set of git mirrors up to date.
 Copyright (c) 2015 Layton Nelson <notyal.dev@gmail.com>
 
-USAGE: $program [OPTION]...
+${h}USAGE${c}: $program [${o}OPTION${c}]...
 
-OPTIONS:
-  archive                Archive the repos.
-  create <url>           Add a repo to the mirror directory.
-  backup-gh-user <user>  Backup all repos associated with a user on Github.
-  delete <repo>          Remove a repo from the mirror directory.
-  list                   List mirrors in the mirror directory.
-    -a, --absolute       Show the absolute path for the location of each mirror.
-  path                   Show the mirror directory location.
-  update                 Update the list of mirrors in the mirror directory.
-  query <user>           Query the Github API for a list of repos associated
-                         with the provided user.
-  help                   Show this help.
+${h}OPTIONS${c}:
+  ${o}archive${c} $i$i$i$i               Archive the repos.
+  ${o}create${c} <${a}url${c}>           Add a repo to the mirror directory.
+  ${o}backup-gh-user${c} <${a}user${c}>  Backup all repos associated with a user on Github.
+  ${o}delete${c} <${a}repo${c}>          Remove a repo from the mirror directory.
+  ${o}list${c}    $i$i$i$i               List mirrors in the mirror directory.
+    -${e}a${c}, --${e}absolute${c}       Show the absolute path for the location of each mirror.
+  ${o}path${c}    $i$i$i$i               Show the mirror directory location.
+  ${o}update${c}  $i$i$i$i               Update the list of mirrors in the mirror directory.
+  ${o}query${c} <${a}user${c}>           Query the Github API for a list of repos associated
+                 $i$i$i$i$i$i$i$i        with the provided user.
+  ${o}help${c}   $i$i$i$i                Show this help.
 EOF
 	exit 0
 }
@@ -109,14 +144,14 @@ query_github_repos(){
 
 	# retrieve api token for github
 	if [[ -f "$DIR/gh_token" ]]; then
-		>&2 echo "Using Github API token from 'gh_token' local file..."
+		>&2 echo "${cINFO}Using Github API token from${c} '${cDIR}gh_token${c}' ${cINFO}local file...${c}"
 		GITHUB_API_TOKEN=$(<"$DIR"/gh_token)
 	else
 		local github_user=$(git config --get github.user)
 		local github_token=$(git config --get github.token)
 
 		if [[ ! -z "$github_user" && ! -z "$github_token" ]]; then
-			>&2 echo "Using Github API token from Git config..."
+			>&2 echo "${cINFO}Using Github API token from Git config...${c}"
 			GITHUB_API_TOKEN="${github_user}:${github_token}"
 		fi
 	fi
@@ -134,7 +169,7 @@ query_github_repos(){
 	if [[ $http_code == 200 ]]; then
 		sed -n 's/.*"clone_url": "\(.*\)".*/\1/p' <<< "$repo_data"
 	else
-		catch_failure "Got HTTP error '$http_code' when trying to fetch from Github API."
+		catch_failure "${cERROR}Got HTTP error${c} '${cINFO}$http_code${c}' ${cERROR}when trying to fetch from Github API.${c}"
 		return 1
 	fi
 
@@ -142,19 +177,19 @@ query_github_repos(){
 }
 
 update_mirror_remote(){
-	echo "@ Updating repo from remote..."
+	echo "${_m} ${cINFO}Updating repo from remote...${c}"
 	git remote update || >&2 echo "$WARN_cannot_update_remote"
 	echo
 }
 
 update_mirrors(){
-	echo "@ Looking for repos in directory: '$DIR'..."
+	echo "${_m} ${cINFO}Looking for repos in directory${c}: '${cDIR}$DIR${c}'${cINFO}...${c}"
 	cd "$DIR" || catch_failure
 	echo
 
 	for repo in $(find_mirrors); do
-		echo "@ Found repo: '$repo'."
-		cd "$repo" || catch_failure "$ERROR_cannot_cd_repo '$repo'."
+		echo "${_m} ${cINFO}Found repo${c}: '${cDIR}$repo${c}'${cINFO}.${c}"
+		cd "$repo" || catch_failure "$ERROR_cannot_cd_repo '${cDIR}$repo${c}'."
 		echo
 
 		update_mirror_remote
@@ -164,7 +199,7 @@ update_mirrors(){
 		#echo
 
 		# done with this repo, go back to top directory
-		echo "\$ Done with '$repo'."
+		echo "${_f} ${cINFO}Done with${c} '${cDIR}$repo${c}'${cINFO}.${c}"
 		echo "$horizontal_line"
 		cd "$DIR" || catch_failure
 		echo
@@ -185,7 +220,7 @@ update_mirrors(){
 # 	#echo "# DEBUG: repo is: '$repo' or '${repo##*/}'."
 # 	#echo "# DEBUG: current dir is: '$PWD' or '${PWD##*/}'."
 #
-# 	echo "@ Archiving current repo..."
+# 	echo "${_m} ${cINFO}Archiving current repo...${c}"
 # 	cd ..
 # 	#tar -cvpzf
 # }
@@ -210,12 +245,12 @@ create_repo(){
 	# remove repo folder from the array
 	unset url_array[${#url_array[@]}-1]
 
-	echo "@ Creating repo for \"$1\"..."
+	echo "${_m} ${cINFO}Creating repo for${c} '${cDIR}$1${c}'${cINFO}...${c}"
 
 	# basically, mkdir -p DOMAIN/DIR for repo url
 	for folder in ${url_array[@]}; do
 		if [[ ! -d "$folder" ]]; then
-			echo "@ Creating folder: '$folder' ..."
+			echo "${_m} ${cINFO}Creating folder${c}: '${cDIR}$folder${c}'${cINFO}...${c}"
 			mkdir "$folder" || catch_failure
 		fi
 		cd "$folder" || catch_failure
@@ -223,22 +258,22 @@ create_repo(){
 	echo
 
 	if [[ ! -d "$repo_folder" ]]; then
-		echo "@ Cloning repo for \"$1\"..."
+		echo "${_m} Cloning repo for \"$1\"..."
 		git clone --mirror "$repo_url" "$repo_folder" || catch_failure
 		cd "$repo_folder" || catch_failure
 		echo
 
-		echo "@ Running git-gc to save space..."
+echo "${_m} ${cINFO}Running git-gc to save space...${c}"
 		git gc
 		echo
 	else
-		echo "NOTE: Repo has already been cloned or the folder already exists..."
+		echo "${cWARN}NOTE: Repo has already been cloned or the folder already exists...${c}"
 		echo
 		update_mirror_remote
 	fi
 
 	# done with this repo, go back to top directory
-	echo "\$ Done with '$repo_folder."
+	echo "${_f} ${cINFO}Done with${c} '${cDIR}$repo_folder'${cDIR}.${c}"
 	echo "$horizontal_line"
 	cd "$DIR" || catch_failure
 }
